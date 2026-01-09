3c5f99ebc3086fa1c3b66996a45bbe96b0fd75b6
germanklang <germanklang@gmail.com>
2026-01-09 12:34:29 +0800

    Buchungen

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-const times = ["13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","20:00","20:30""21:00","21:30",];
+const times = [
+  "13:00","13:30","14:00","14:30","15:00","15:30",
+  "16:00","16:30","17:00","17:30","18:00","18:30",
+  "20:00","20:30","21:00","21:30"
+];
 
 const dateSel = document.getElementById("date");
 const timeSel = document.getElementById("time");
@@ -22,31 +26,18 @@ function book() {
 
   fetch("https://script.google.com/macros/s/AKfycbz9AoMsaK5gY8nNnMBsUggqUQHQ8p35XOzYEAYw3BLedJnsDKqh5rTm5njxEsiIG5OC/exec", {
     method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
     body: JSON.stringify(data)
   })
-  .then(() => {
+  .then(res => res.text())
+  .then(msg => {
+    console.log("Server:", msg);
     document.getElementById("msg").innerText = "Booked!";
+  })
+  .catch(err => {
+    console.error(err);
+    document.getElementById("msg").innerText = "Error!";
   });
 }
-function doGet() {
-  const sheet = SpreadsheetApp.getActive().getSheetByName("Sheet1");
-  const data = sheet.getDataRange().getValues();
-  return ContentService.createTextOutput(JSON.stringify(data))
-    .setMimeType(ContentService.MimeType.JSON);
-  etch("DEINE_WEBAPP_URL", {
-  method: "POST",
-  body: JSON.stringify({
-    time: selectedTime,
-    name: userName,
-    email: userEmail
-  }),
-  headers: {
-    'Content-Type': 'application/json'
-  }
-})
-.then(res => res.text())
-.then(res => console.log("Buchung gesendet:", res))
-.catch(err => console.error(err));
-
-}
-
